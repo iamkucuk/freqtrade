612822cc2fbbb62acb96f234a46344130d2b3e56
Furkan Kucuk <furkankucuk@outlook.com>
2024-07-21 15:30:33 +0300

    chore: Update run_docker.sh script for running the Freqtrade development container

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,13 @@
-docker rm -f freqtrade_develop
-
-docker run -it \
-    --name "freqtrade_develop" \
-    --publish 8080:8080 \
-    --mount type=bind,source="$(pwd)",target=/freqtrade,consistency=cached \
-    --user "ftuser" \
-    ghcr.io/freqtrade/freqtrade-devcontainer:latest \
-    /bin/bash -c "pip install --user -e . && freqtrade create-userdir --userdir user_data/ && pip install -r tuneta/requirements.txt && /bin/bash"
+if docker ps -a | grep -q freqtrade_develop; then
+    echo "Container freqtrade_develop exists, starting it..."
+    docker start freqtrade_develop
+else
+    echo "Container freqtrade_develop does not exist, creating and starting a new one..."
+    docker run -it \
+        --name "freqtrade_develop" \
+        --publish 8080:8080 \
+        --mount type=bind,source="$(pwd)",target=/freqtrade,consistency=cached \
+        --user "ftuser" \
+        ghcr.io/freqtrade/freqtrade-devcontainer:latest \
+        /bin/bash -c "pip install --user -e . && freqtrade create-userdir --userdir user_data/ && pip install -r tuneta/requirements.txt && /bin/bash"
+fi
